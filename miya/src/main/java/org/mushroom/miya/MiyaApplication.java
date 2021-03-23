39f35dd15ea7b59701d08b29e1cf9f25645d74b6
zhouchenpo <zhouchenpo@zju.edu.cn>
2021-03-23 23:07:18 +0800

    ini

---
+++ miya/src/main/java/org/mushroom/miya/MiyaApplication.java
@@ -0,0 +1,13 @@
+package org.mushroom.miya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiyaApplication.class, args);
+    }
+
+}
---
+++ miya/src/test/java/org/mushroom/miya/MiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mushroom.miya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
